fe8d2b8e166b4438760a821f6a5a279a5c15a178
Maria Antonieta Pereira <antonietap0@gmail.com>
2022-09-14 19:48:32 -0500

    exercise rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Defines the root path route ('/')
+  # root 'articles#index'
+  # Un visitante puede ver la lista de todos los restaurantes.
+ # get '/restaurants', to: 'restaurants#index'
+  # Un visitante puede agregar un nuevo restaurante y ser redireccionado a la vista show de ese nuevo restaurante.
+  # get 'restaurants/new', to: 'restaurants#new', as: :new
+  # post 'restaurants', to: 'restaurants#create'
+  # Un visitante puede ver los detalles de un restaurante con todos sus reviews.
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # Un visitante puede agregar un review a un restaurante.
+  # get 'restaurants/:id/reviews/new', to: 'reviews#new', as: :review_new
+  # post 'restaurants/:id/reviews', to: 'reviews#create'
+  resources :restaurants, only: [:new, :index, :show, :create, :edit, :destroy, :update] do
+    resources :reviews, only: [:new, :create]
+  end
+    resources :reviews, only: [:destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.destroy_all
+20.git addtimes do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_name,
+    phone_number: Faker::Number.number(digits: 10),
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+  )
+  5.times do
+    Review.create(
+      content: Faker::Lorem.paragraph,
+      rating: rand(1..5),
+      # restaurant_id: Restaurant.last.id
+      restaurant: restaurant
+    )
+  end
+end
